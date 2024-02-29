707ea629462c53e27a3ecd8f6d40e3949d62bdf7
misraved <ved@turbot.com>
2024-02-29 12:33:42 +0530

    Add brand color

--- mod.pp
+++ mod.pp
@@ -1,6 +1,7 @@
 mod "imdb" {
   title         = "IMDb"
   description   = "Visualize key metrics and trends in the movie industry, including financial performance, audience preferences, and critical acclaim, leveraging SQL queries to analyze IMDb data using Powerpipe."
+  color         = "#F5C518"
   documentation = file("./README.md")
   categories    = ["sqlite"]
 }
