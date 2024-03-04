62f36080251acd917b92b0cf86847381ab318c38
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:31:18 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -3,5 +3,5 @@ mod "imdb" {
   description   = "Visualize key metrics and trends in the movie industry, including financial performance, audience preferences, and critical acclaim, leveraging SQL queries to analyze IMDb data using Powerpipe."
   color         = "#F5C518"
   documentation = file("./README.md")
-  categories    = ["sqlite"]
+  categories    = ["dashboard", "sqlite"]
 }
