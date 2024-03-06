a7f120a3c98bace1a4bff0894a25b74cf9fd56d0
sourav chakraborty <sourav@turbot.com>
2024-03-06 16:59:29 +0530

    update

--- mod.pp
+++ mod.pp
@@ -4,10 +4,10 @@ mod "imdb" {
   color         = "#F5C518"
   documentation = file("./README.md")
   categories    = ["dashboard", "sqlite"]
-}
 
-opengraph {
-  title       = "Powerpipe Mod for IMDb"
-  description = "Visualize key metrics and trends in the movie industry, including financial performance, audience preferences, and critical acclaim, leveraging SQL queries to analyze IMDb data using Powerpipe."
-  image       = "/images/mods/turbot/imdb-social-graphic.png"
+  opengraph {
+    title       = "Powerpipe Mod for IMDb"
+    description = "Visualize key metrics and trends in the movie industry, including financial performance, audience preferences, and critical acclaim, leveraging SQL queries to analyze IMDb data using Powerpipe."
+    image       = "/images/mods/turbot/imdb-social-graphic.png"
+  }
 }
